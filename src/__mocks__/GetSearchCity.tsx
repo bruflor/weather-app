d73b297e4327a5a83743abff3897328b9ea30b76
Bruna Flor <brn.flor@gmail.com>
2022-11-10 11:35:44 +0000

    mock data searched city

---
+++ src/__mocks__/GetSearchCity.tsx
@@ -0,0 +1,12 @@
+export const GetSearchCity = () => {
+  return Promise.resolve({
+    AdministrativeArea: {
+      EnglishName: "Narnia",
+    },
+    Country: {
+      EnglishName: "Ettinsmoor",
+    },
+    Key: "12364",
+    LocalizedName: "Narnia",
+  });
+};
